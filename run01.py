c0f221fe7734fcf32940c639cbb022fdfea4403d
rankrizberg <73697749+rankrizberg@users.noreply.github.com>
2021-11-03 21:11:37 +0200

    ddd
    ddd

--- run01.py
+++ run01.py
@@ -1,13 +1,10 @@
 import csv
-
-with open('ftpsrv.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
+file = open("ftpsrv.csv")
+csvreader = csv.reader(file)
+header = next(csvreader)
+print(header)
+rows = []
+for row in csvreader:
+    rows.append(row)
+print(rows)
+file.close()
